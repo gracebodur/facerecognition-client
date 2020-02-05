a91160a2d6921c90f27bc3e7fcf0ff3c7d321823
Grace <mgracebodur@gmail.com>
2020-02-05 00:54:22 -0500

    Created basic components and style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+
 import './App.css';
 
-function App() {
+const particlesOptions = {
+  particles: {
+   number: {
+     value: 30,
+     density: {
+       enable: true,
+       value_area: 800
+     }
+   }
+  }
+}
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-          Hello World!
-      </header>
+      <Particles className='particles'
+          params={particlesOptions}
+        />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
+}
 }
 
 export default App;
